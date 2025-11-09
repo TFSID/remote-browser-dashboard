ea63dfeeed68078d73d39da9629b59d0fe9ff26a
[dyad] <tegar.firman215@gmail.com>
2025-11-09 18:30:16 +0700

    [dyad] Changed the output area layout to a single vertical column using Flexbox - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,7 +53,7 @@ export default function Home() {
         />
         
         {/* Output Area: Logs and Browser View */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="flex flex-col gap-5">
           <LogsPanel
             logs={logs}
             logContainerRef={logContainerRef}
