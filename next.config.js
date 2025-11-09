84da1ab10370a1d9c822f59091df803c7bb32933
[dyad] <tegar.firman215@gmail.com>
2025-11-09 17:27:00 +0700

    [dyad] Fixing Next.js configuration syntax - wrote 1 file(s) + extra files edited outside of Dyad

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    if (process.env.NODE_ENV === "development") {
+      config.module.rules.push({
+        test: /\.(jsx|tsx)$/,
+        exclude: /node_modules/,
+        enforce: "pre",
+        use: "@dyad-sh/nextjs-webpack-component-tagger",
+      });
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
